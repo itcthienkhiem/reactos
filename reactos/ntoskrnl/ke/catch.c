itcthienkhiem/reactos

commit 10fd658bf8012ef520a44b5c9b16b34a7269184b

- Remove unused ldr/userldr.c - Rename LdrLoadInitialProcess to Exp... and move to executive initialization. - Removed ldr/init.c - Renamed ldr/sysdll.c functions to Psp (correct naming) and deleted file. - Renamed SystemDll... ntdll pointers to correct Ke names - Use direct Ke names instead of going through an API call. - Make ExpInit... functions STDCALL
svn path=/trunk/; revision=17053

reactos/ntoskrnl/ke/catch.c
@@ -161,7 +161,7 @@ KiDispatchException(PEXCEPTION_RECORD ExceptionRecord,
                     DPRINT1("User-mode stack was invalid. Terminating target thread\n");
                 }
                 /* Set EIP to the User-mode Dispathcer */
-                Tf->Eip = (ULONG)LdrpGetSystemDllExceptionDispatcher();
+                Tf->Eip = (ULONG)KeRaiseUserExceptionDispatcher;
                 return;
             }
 

reactos/ntoskrnl/ex/callback.c
@@ -71,6 +71,8 @@ KEVENT ExpCallbackEvent;
  * TRUE if the Callback Object Type was successfully created.
  */
 VOID
+INIT_FUNCTION
+STDCALL
 ExpInitializeCallbacks(VOID)
 {
    OBJECT_ATTRIBUTES ObjectAttributes;

reactos/ntoskrnl/ex/event.c
@@ -34,6 +34,7 @@ static const INFORMATION_CLASS_INFO ExEventInfoClass[] = {
 
 VOID
 INIT_FUNCTION
+STDCALL
 ExpInitializeEventImplementation(VOID)
 {
   OBJECT_TYPE_INITIALIZER ObjectTypeInitializer;

reactos/ntoskrnl/ex/evtpair.c
@@ -31,6 +31,7 @@ static GENERIC_MAPPING ExEventPairMapping = {
 
 VOID
 INIT_FUNCTION
+STDCALL
 ExpInitializeEventPairImplementation(VOID)
 {
   OBJECT_TYPE_INITIALIZER ObjectTypeInitializer;

reactos/ntoskrnl/ex/init.c
@@ -405,6 +405,79 @@ ExpDisplayNotice(VOID)
     HalDisplayString(str);
     
 }
+
+INIT_FUNCTION
+NTSTATUS
+ExpLoadInitialProcess(PHANDLE ProcessHandle,
+                      PHANDLE ThreadHandle)
+{
+    UNICODE_STRING ImagePath = RTL_CONSTANT_STRING(L"\\SystemRoot\\system32\\smss.exe");
+    HANDLE SystemProcessHandle;
+    NTSTATUS Status;
+    PRTL_USER_PROCESS_PARAMETERS Params=NULL;
+    RTL_USER_PROCESS_INFORMATION Info;
+
+    /* Create a handle to the process */
+    Status = ObpCreateHandle(PsGetCurrentProcess(),
+                             PsInitialSystemProcess,
+                             PROCESS_CREATE_PROCESS | PROCESS_CREATE_THREAD | PROCESS_QUERY_INFORMATION,
+                             FALSE,
+                             &SystemProcessHandle);
+    if(!NT_SUCCESS(Status))
+    {
+        DPRINT1("Failed to create a handle for the system process!\n");
+        return Status;
+    }
+
+    /* Create the Parameters */
+    Status = RtlCreateProcessParameters(&Params,
+                                        &ImagePath,
+                                        NULL,
+                                        NULL,
+                                        NULL,
+                                        NULL,
+                                        NULL,
+                                        NULL,
+                                        NULL,
+                                        NULL);
+    if(!NT_SUCCESS(Status))
+    {
+        DPRINT1("Failed to create ppb!\n");
+        ZwClose(SystemProcessHandle);
+        return Status;
+    }
+
+    DPRINT("Creating process\n");
+    Status = RtlCreateUserProcess(&ImagePath,
+                                  OBJ_CASE_INSENSITIVE,
+                                  Params,
+                                  NULL,
+                                  NULL,
+                                  SystemProcessHandle,
+                                  FALSE,
+                                  NULL,
+                                  NULL,
+                                  &Info);
+    
+    /* Close the handle and free the params */
+    ZwClose(SystemProcessHandle);
+    RtlDestroyProcessParameters(Params);
+
+    if (!NT_SUCCESS(Status))
+    {
+        DPRINT1("NtCreateProcess() failed (Status %lx)\n", Status);
+        return(Status);
+    }
+
+    /* Start it up */
+    ZwResumeThread(Info.ThreadHandle, NULL);
+
+    /* Return Handles */
+    *ProcessHandle = Info.ProcessHandle;
+    *ThreadHandle = Info.ThreadHandle;
+    DPRINT("Process created successfully\n");
+    return STATUS_SUCCESS;
+}
    
 VOID
 INIT_FUNCTION
@@ -468,7 +541,7 @@ ExpInitializeExecutive(VOID)
     ObInit();
 
     /* Initialize Lookaside Lists */
-    ExInit2();
+    ExpInitLookasideLists();
 
     /* Set up Region Maps, Sections and the Paging File */
     MmInit2();
@@ -506,7 +579,7 @@ ExpInitializeExecutive(VOID)
     HalInitSystem(1, (PLOADER_PARAMETER_BLOCK)&KeLoaderBlock);
 
     /* Initialize Basic System Objects and Worker Threads */
-    ExInit3();
+    ExInit2();
 
     /* Create the system handle table, assign it to the system process, create
        the client id table and assign a PID for the system process. This needs
@@ -575,7 +648,7 @@ ExpInitializeExecutive(VOID)
     IoInit3();
 
     /* Load the System DLL and its Entrypoints */
-    LdrpInitializeSystemDll();
+    PsLocateSystemDll();
 
     /* Initialize the Default Locale */
     PiInitDefaultLocale();
@@ -604,7 +677,7 @@ ExpInitializeExecutive(VOID)
     }
 
     /* Launch initial process */
-    Status = LdrLoadInitialProcess(&ProcessHandle,
+    Status = ExpLoadInitialProcess(&ProcessHandle,
                                    &ThreadHandle);
 
     /* Check for success, Bugcheck if we failed */
@@ -671,25 +744,21 @@ ExpInitializeExecutive(VOID)
     ZwClose(ProcessHandle);
 }
 
-VOID INIT_FUNCTION
+VOID
+STDCALL
+INIT_FUNCTION
 ExInit2(VOID)
 {
-  ExpInitLookasideLists();
-}
-
-VOID INIT_FUNCTION
-ExInit3 (VOID)
-{
-  ExpInitializeEventImplementation();
-  ExpInitializeEventPairImplementation();
-  ExpInitializeMutantImplementation();
-  ExpInitializeSemaphoreImplementation();
-  ExpInitializeTimerImplementation();
-  LpcpInitSystem();
-  ExpInitializeProfileImplementation();
-  ExpWin32kInit();
-  ExpInitUuids();
-  ExpInitializeHandleTables();
+    ExpInitializeEventImplementation();
+    ExpInitializeEventPairImplementation();
+    ExpInitializeMutantImplementation();
+    ExpInitializeSemaphoreImplementation();
+    ExpInitializeTimerImplementation();
+    LpcpInitSystem();
+    ExpInitializeProfileImplementation();
+    ExpWin32kInit();
+    ExpInitUuids();
+    ExpInitializeHandleTables();
 }
 
 /* EOF */

reactos/ntoskrnl/ex/lookas.c
@@ -26,6 +26,7 @@ KSPIN_LOCK ExpPagedLookasideListLock;
 
 VOID 
 INIT_FUNCTION
+STDCALL
 ExpInitLookasideLists()
 {
     /* Initialize Lock and Listhead */

reactos/ntoskrnl/ex/mutant.c
@@ -51,6 +51,7 @@ ExpDeleteMutant(PVOID ObjectBody)
 
 VOID
 INIT_FUNCTION
+STDCALL
 ExpInitializeMutantImplementation(VOID)
 {
     OBJECT_TYPE_INITIALIZER ObjectTypeInitializer;

reactos/ntoskrnl/ex/profile.c
@@ -74,6 +74,7 @@ ExpDeleteProfile(PVOID ObjectBody)
 
 VOID
 INIT_FUNCTION
+STDCALL
 ExpInitializeProfileImplementation(VOID)
 {
     OBJECT_TYPE_INITIALIZER ObjectTypeInitializer;

commit 056ac3dfd4a8d3f6d009246dd6ddd9cc2334c90c

Call the correct user mode dispatch function in KiDispatchException. The bug was introduced by r17053.
svn path=/trunk/; revision=17558

reactos/ntoskrnl/ke/catch.c
@@ -161,7 +161,7 @@ KiDispatchException(PEXCEPTION_RECORD ExceptionRecord,
                     DPRINT1("User-mode stack was invalid. Terminating target thread\n");
                 }
                 /* Set EIP to the User-mode Dispathcer */
-                Tf->Eip = (ULONG)KeRaiseUserExceptionDispatcher;
+                Tf->Eip = (ULONG)KeUserExceptionDispatcher;
                 return;
             }
 