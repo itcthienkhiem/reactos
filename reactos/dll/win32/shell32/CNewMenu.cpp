itcthienkhiem/reactos

commit 62d04fb9d3bc4119468b79672e4ebf8a95ac4862

[SHELL32]: Fix the "CNewMenu.cpp:432: Unexpected failure 80070002." debug line that appears when one tries to create a new file using the explorer (note: did not happen for new directories). CORE-12684 #resolve #comment Fixed in r73579.
svn path=/trunk/; revision=73579

reactos/dll/win32/shell32/CNewMenu.cpp
@@ -530,7 +530,9 @@ HRESULT CNewMenu::CreateNewItem(SHELLNEW_ITEM *pItem, LPCMINVOKECOMMANDINFO lpcm
 
             /* Build new file name */
             LoadStringW(shell32_hInstance, FCIDM_SHVIEW_NEW, wszBuf, _countof(wszBuf));
-            StringCchCatExW(wszPath, _countof(wszPath), L"\\", &pwszFilename, &cchFilenameMax, 0);
+            // FIXME: PathCchAddBackslashExW(wszPath, _countof(wszPath), &pwszFilename, &cchFilenameMax);
+            pwszFilename = PathAddBackslashW(wszPath);
+            cchFilenameMax = wcslen(wszPath) + 1;
             StringCchPrintfW(pwszFilename, cchFilenameMax, L"%s %s%s", wszBuf, pItem->pwszDesc, pItem->pwszExt);
 
             /* Find unique name */

commit 0e6391ea5e6fddbb4f0d21ba45f2e1a012d31338

Fix a bug introduced in r73579. CORE-12684
svn path=/trunk/; revision=73580

reactos/dll/win32/shell32/CNewMenu.cpp
@@ -532,7 +532,7 @@ HRESULT CNewMenu::CreateNewItem(SHELLNEW_ITEM *pItem, LPCMINVOKECOMMANDINFO lpcm
             LoadStringW(shell32_hInstance, FCIDM_SHVIEW_NEW, wszBuf, _countof(wszBuf));
             // FIXME: PathCchAddBackslashExW(wszPath, _countof(wszPath), &pwszFilename, &cchFilenameMax);
             pwszFilename = PathAddBackslashW(wszPath);
-            cchFilenameMax = wcslen(wszPath) + 1;
+            cchFilenameMax = _countof(wszBuf) - wcslen(wszPath);
             StringCchPrintfW(pwszFilename, cchFilenameMax, L"%s %s%s", wszBuf, pItem->pwszDesc, pItem->pwszExt);
 
             /* Find unique name */