itcthienkhiem/reactos

commit 604214e9d6487c1cca371dd9f523e17526a701f1

[GDI32_APITEST]: Fix compilation under MSVC 2010.
svn path=/trunk/; revision=74962

rostests/apitests/gdi32/AddFontMemResourceEx.c
@@ -32,161 +32,161 @@ typedef struct _fnt_test
 static fnt_test test_data[] =
 {
     {
-        .ResourceName = "PanosePitchTest.ttf",
-        .NumFaces = 2,
-        .res =
+        /* .ResourceName = */ "PanosePitchTest.ttf",
+        /* .NumFaces = */ 2,
+        /* .res = */
         {
             {
-                .FontName = "PanosePitchTest",
-                .tm.tmHeight = 11,
-                .tm.tmAscent = 11,
-                .tm.tmDescent = 0,
-                .tm.tmInternalLeading = -5,
-                .tm.tmExternalLeading = 1,
-                .tm.tmAveCharWidth = 8,
-                .tm.tmMaxCharWidth = 11,
-                .tm.tmWeight = FW_NORMAL,
-                .tm.tmOverhang = 0,
-                .tm.tmDigitizedAspectX = 96,
-                .tm.tmDigitizedAspectY = 96,
-                .tm.tmFirstChar = 63,
-                .tm.tmLastChar = 65,
-                .tm.tmDefaultChar = 165,
-                .tm.tmBreakChar = 65,
-                .tm.tmItalic = 0,
-                .tm.tmUnderlined = 0,
-                .tm.tmStruckOut = 0,
-                .tm.tmPitchAndFamily = TMPF_TRUETYPE | TMPF_VECTOR,
-                .tm.tmCharSet = SHIFTJIS_CHARSET,
+                /* .FontName = */ "PanosePitchTest",
+                /* .tm.tmHeight = */ 11,
+                /* .tm.tmAscent = */ 11,
+                /* .tm.tmDescent = */ 0,
+                /* .tm.tmInternalLeading = */ -5,
+                /* .tm.tmExternalLeading = */ 1,
+                /* .tm.tmAveCharWidth = */ 8,
+                /* .tm.tmMaxCharWidth = */ 11,
+                /* .tm.tmWeight = */ FW_NORMAL,
+                /* .tm.tmOverhang = */ 0,
+                /* .tm.tmDigitizedAspectX = */ 96,
+                /* .tm.tmDigitizedAspectY = */ 96,
+                /* .tm.tmFirstChar = */ 63,
+                /* .tm.tmLastChar = */ 65,
+                /* .tm.tmDefaultChar = */ 165,
+                /* .tm.tmBreakChar = */ 65,
+                /* .tm.tmItalic = */ 0,
+                /* .tm.tmUnderlined = */ 0,
+                /* .tm.tmStruckOut = */ 0,
+                /* .tm.tmPitchAndFamily = */ TMPF_TRUETYPE | TMPF_VECTOR,
+                /* .tm.tmCharSet = */ SHIFTJIS_CHARSET,
             },
             {
-                .FontName = "@PanosePitchTest",
-                .tm.tmHeight = 11,
-                .tm.tmAscent = 11,
-                .tm.tmDescent = 0,
-                .tm.tmInternalLeading = -5,
-                .tm.tmExternalLeading = 1,
-                .tm.tmAveCharWidth = 8,
-                .tm.tmMaxCharWidth = 11,
-                .tm.tmWeight = FW_NORMAL,
-                .tm.tmOverhang = 0,
-                .tm.tmDigitizedAspectX = 96,
-                .tm.tmDigitizedAspectY = 96,
-                .tm.tmFirstChar = 63,
-                .tm.tmLastChar = 65,
-                .tm.tmDefaultChar = 165,
-                .tm.tmBreakChar = 65,
-                .tm.tmItalic = 0,
-                .tm.tmUnderlined = 0,
-                .tm.tmStruckOut = 0,
-                .tm.tmPitchAndFamily = TMPF_TRUETYPE | TMPF_VECTOR,
-                .tm.tmCharSet = SHIFTJIS_CHARSET,
+                /* .FontName = */ "@PanosePitchTest",
+                /* .tm.tmHeight = */ 11,
+                /* .tm.tmAscent = */ 11,
+                /* .tm.tmDescent = */ 0,
+                /* .tm.tmInternalLeading = */ -5,
+                /* .tm.tmExternalLeading = */ 1,
+                /* .tm.tmAveCharWidth = */ 8,
+                /* .tm.tmMaxCharWidth = */ 11,
+                /* .tm.tmWeight = */ FW_NORMAL,
+                /* .tm.tmOverhang = */ 0,
+                /* .tm.tmDigitizedAspectX = */ 96,
+                /* .tm.tmDigitizedAspectY = */ 96,
+                /* .tm.tmFirstChar = */ 63,
+                /* .tm.tmLastChar = */ 65,
+                /* .tm.tmDefaultChar = */ 165,
+                /* .tm.tmBreakChar = */ 65,
+                /* .tm.tmItalic = */ 0,
+                /* .tm.tmUnderlined = */ 0,
+                /* .tm.tmStruckOut = */ 0,
+                /* .tm.tmPitchAndFamily = */ TMPF_TRUETYPE | TMPF_VECTOR,
+                /* .tm.tmCharSet = */ SHIFTJIS_CHARSET,
             },
         },
     },
     {
-        .ResourceName = "TTCTestV.ttc",
-        .NumFaces = 3,
-        .res =
+        /* .ResourceName = */ "TTCTestV.ttc",
+        /* .NumFaces = */ 3,
+        /* .res = */
         {
             {
-                .FontName = "No1Of3in1",
-                .tm.tmHeight = 12,
-                .tm.tmAscent = 12,
-                .tm.tmDescent = 0,
-                .tm.tmInternalLeading = -4,
-                .tm.tmExternalLeading = 1,
-                .tm.tmAveCharWidth = -525,
-                .tm.tmMaxCharWidth = 6,
-                .tm.tmWeight = FW_NORMAL,
-                .tm.tmOverhang = 0,
-                .tm.tmDigitizedAspectX = 96,
-                .tm.tmDigitizedAspectY = 96,
-                .tm.tmFirstChar = 63,
-                .tm.tmLastChar = 65,
-                .tm.tmDefaultChar = 64,
-                .tm.tmBreakChar = 65,
-                .tm.tmItalic = 0,
-                .tm.tmUnderlined = 0,
-                .tm.tmStruckOut = 0,
-                .tm.tmPitchAndFamily = TMPF_TRUETYPE | TMPF_VECTOR | TMPF_FIXED_PITCH,
-                .tm.tmCharSet = ANSI_CHARSET,
+                /* .FontName = */ "No1Of3in1",
+                /* .tm.tmHeight = */ 12,
+                /* .tm.tmAscent = */ 12,
+                /* .tm.tmDescent = */ 0,
+                /* .tm.tmInternalLeading = */ -4,
+                /* .tm.tmExternalLeading = */ 1,
+                /* .tm.tmAveCharWidth = */ -525,
+                /* .tm.tmMaxCharWidth = */ 6,
+                /* .tm.tmWeight = */ FW_NORMAL,
+                /* .tm.tmOverhang = */ 0,
+                /* .tm.tmDigitizedAspectX = */ 96,
+                /* .tm.tmDigitizedAspectY = */ 96,
+                /* .tm.tmFirstChar = */ 63,
+                /* .tm.tmLastChar = */ 65,
+                /* .tm.tmDefaultChar = */ 64,
+                /* .tm.tmBreakChar = */ 65,
+                /* .tm.tmItalic = */ 0,
+                /* .tm.tmUnderlined = */ 0,
+                /* .tm.tmStruckOut = */ 0,
+                /* .tm.tmPitchAndFamily = */ TMPF_TRUETYPE | TMPF_VECTOR | TMPF_FIXED_PITCH,
+                /* .tm.tmCharSet = */ ANSI_CHARSET,
             },
             {
-                .FontName = "No2Of3in1",
-                .tm.tmHeight = 12,
-                .tm.tmAscent = 12,
-                .tm.tmDescent = 0,
-                .tm.tmInternalLeading = -4,
-                .tm.tmExternalLeading = 1,
-                .tm.tmAveCharWidth = 8,
-                .tm.tmMaxCharWidth = 7,
-                .tm.tmWeight = FW_NORMAL,
-                .tm.tmOverhang = 0,
-                .tm.tmDigitizedAspectX = 96,
-                .tm.tmDigitizedAspectY = 96,
-                .tm.tmFirstChar = 63,
-                .tm.tmLastChar = 65,
-                .tm.tmDefaultChar = 64,
-                .tm.tmBreakChar = 65,
-                .tm.tmItalic = 0,
-                .tm.tmUnderlined = 0,
-                .tm.tmStruckOut = 0,
-                .tm.tmPitchAndFamily = TMPF_TRUETYPE | TMPF_VECTOR | TMPF_FIXED_PITCH,
-                .tm.tmCharSet = ANSI_CHARSET,
+                /* .FontName = */ "No2Of3in1",
+                /* .tm.tmHeight = */ 12,
+                /* .tm.tmAscent = */ 12,
+                /* .tm.tmDescent = */ 0,
+                /* .tm.tmInternalLeading = */ -4,
+                /* .tm.tmExternalLeading = */ 1,
+                /* .tm.tmAveCharWidth = */ 8,
+                /* .tm.tmMaxCharWidth = */ 7,
+                /* .tm.tmWeight = */ FW_NORMAL,
+                /* .tm.tmOverhang = */ 0,
+                /* .tm.tmDigitizedAspectX = */ 96,
+                /* .tm.tmDigitizedAspectY = */ 96,
+                /* .tm.tmFirstChar = */ 63,
+                /* .tm.tmLastChar = */ 65,
+                /* .tm.tmDefaultChar = */ 64,
+                /* .tm.tmBreakChar = */ 65,
+                /* .tm.tmItalic = */ 0,
+                /* .tm.tmUnderlined = */ 0,
+                /* .tm.tmStruckOut = */ 0,
+                /* .tm.tmPitchAndFamily = */ TMPF_TRUETYPE | TMPF_VECTOR | TMPF_FIXED_PITCH,
+                /* .tm.tmCharSet = */ ANSI_CHARSET,
             },
             {
-                .FontName = "No3Of3in1V",
-                .tm.tmHeight = 12,
-                .tm.tmAscent = 12,
-                .tm.tmDescent = 0,
-                .tm.tmInternalLeading = -4,
-                .tm.tmExternalLeading = 1,
-                .tm.tmAveCharWidth = 8,
-                .tm.tmMaxCharWidth = 13,
-                .tm.tmWeight = FW_NORMAL,
-                .tm.tmOverhang = 0,
-                .tm.tmDigitizedAspectX = 96,
-                .tm.tmDigitizedAspectY = 96,
-                .tm.tmFirstChar = 63,
-                .tm.tmLastChar = 65,
-                .tm.tmDefaultChar = 64,
-                .tm.tmBreakChar = 65,
-                .tm.tmItalic = 0,
-                .tm.tmUnderlined = 0,
-                .tm.tmStruckOut = 0,
-                .tm.tmPitchAndFamily = FF_MODERN | TMPF_TRUETYPE | TMPF_VECTOR,
-                .tm.tmCharSet = ANSI_CHARSET,
+                /* .FontName = */ "No3Of3in1V",
+                /* .tm.tmHeight = */ 12,
+                /* .tm.tmAscent = */ 12,
+                /* .tm.tmDescent = */ 0,
+                /* .tm.tmInternalLeading = */ -4,
+                /* .tm.tmExternalLeading = */ 1,
+                /* .tm.tmAveCharWidth = */ 8,
+                /* .tm.tmMaxCharWidth = */ 13,
+                /* .tm.tmWeight = */ FW_NORMAL,
+                /* .tm.tmOverhang = */ 0,
+                /* .tm.tmDigitizedAspectX = */ 96,
+                /* .tm.tmDigitizedAspectY = */ 96,
+                /* .tm.tmFirstChar = */ 63,
+                /* .tm.tmLastChar = */ 65,
+                /* .tm.tmDefaultChar = */ 64,
+                /* .tm.tmBreakChar = */ 65,
+                /* .tm.tmItalic = */ 0,
+                /* .tm.tmUnderlined = */ 0,
+                /* .tm.tmStruckOut = */ 0,
+                /* .tm.tmPitchAndFamily = */ FF_MODERN | TMPF_TRUETYPE | TMPF_VECTOR,
+                /* .tm.tmCharSet = */ ANSI_CHARSET,
             },
         },
     },
     {
-        .ResourceName = "Shadows_Into_Light.ttf",
-        .NumFaces = 1,
-        .res =
+        /* .ResourceName = */ "Shadows_Into_Light.ttf",
+        /* .NumFaces = */ 1,
+        /* .res = */
         {
             {
-                .FontName = "ufaXaAlLOxCUGYJ7KN51UP2Q==",
-                .tm.tmHeight = 26,
-                .tm.tmAscent = 19,
-                .tm.tmDescent = 7,
-                .tm.tmInternalLeading = 10,
-                .tm.tmExternalLeading = 0,
-                .tm.tmAveCharWidth = 7,
-                .tm.tmMaxCharWidth = 23,
-                .tm.tmWeight = FW_NORMAL,
-                .tm.tmOverhang = 0,
-                .tm.tmDigitizedAspectX = 96,
-                .tm.tmDigitizedAspectY = 96,
-                .tm.tmFirstChar = 30,
-                .tm.tmLastChar = 255,
-                .tm.tmDefaultChar = 31,
-                .tm.tmBreakChar = 32,
-                .tm.tmItalic = 0,
-                .tm.tmUnderlined = 0,
-                .tm.tmStruckOut = 0,
-                .tm.tmPitchAndFamily = TMPF_TRUETYPE | TMPF_VECTOR | TMPF_FIXED_PITCH,
-                .tm.tmCharSet = ANSI_CHARSET,
+                /* .FontName = */ "ufaXaAlLOxCUGYJ7KN51UP2Q==",
+                /* .tm.tmHeight = */ 26,
+                /* .tm.tmAscent = */ 19,
+                /* .tm.tmDescent = */ 7,
+                /* .tm.tmInternalLeading = */ 10,
+                /* .tm.tmExternalLeading = */ 0,
+                /* .tm.tmAveCharWidth = */ 7,
+                /* .tm.tmMaxCharWidth = */ 23,
+                /* .tm.tmWeight = */ FW_NORMAL,
+                /* .tm.tmOverhang = */ 0,
+                /* .tm.tmDigitizedAspectX = */ 96,
+                /* .tm.tmDigitizedAspectY = */ 96,
+                /* .tm.tmFirstChar = */ 30,
+                /* .tm.tmLastChar = */ 255,
+                /* .tm.tmDefaultChar = */ 31,
+                /* .tm.tmBreakChar = */ 32,
+                /* .tm.tmItalic = */ 0,
+                /* .tm.tmUnderlined = */ 0,
+                /* .tm.tmStruckOut = */ 0,
+                /* .tm.tmPitchAndFamily = */ TMPF_TRUETYPE | TMPF_VECTOR | TMPF_FIXED_PITCH,
+                /* .tm.tmCharSet = */ ANSI_CHARSET,
             },
         },
     },

commit 1db45956baea1aa93c0d5a22af9da08349f4fc83

[GDI32_APITEST]: Fix the "missing braces around initializer" GCC error that I've introduced in r74962.
svn path=/trunk/; revision=74966

rostests/apitests/gdi32/AddFontMemResourceEx.c
@@ -38,6 +38,7 @@ static fnt_test test_data[] =
         {
             {
                 /* .FontName = */ "PanosePitchTest",
+                {
                 /* .tm.tmHeight = */ 11,
                 /* .tm.tmAscent = */ 11,
                 /* .tm.tmDescent = */ 0,
@@ -58,9 +59,11 @@ static fnt_test test_data[] =
                 /* .tm.tmStruckOut = */ 0,
                 /* .tm.tmPitchAndFamily = */ TMPF_TRUETYPE | TMPF_VECTOR,
                 /* .tm.tmCharSet = */ SHIFTJIS_CHARSET,
+                }
             },
             {
                 /* .FontName = */ "@PanosePitchTest",
+                {
                 /* .tm.tmHeight = */ 11,
                 /* .tm.tmAscent = */ 11,
                 /* .tm.tmDescent = */ 0,
@@ -81,6 +84,7 @@ static fnt_test test_data[] =
                 /* .tm.tmStruckOut = */ 0,
                 /* .tm.tmPitchAndFamily = */ TMPF_TRUETYPE | TMPF_VECTOR,
                 /* .tm.tmCharSet = */ SHIFTJIS_CHARSET,
+                }
             },
         },
     },
@@ -91,6 +95,7 @@ static fnt_test test_data[] =
         {
             {
                 /* .FontName = */ "No1Of3in1",
+                {
                 /* .tm.tmHeight = */ 12,
                 /* .tm.tmAscent = */ 12,
                 /* .tm.tmDescent = */ 0,
@@ -111,9 +116,11 @@ static fnt_test test_data[] =
                 /* .tm.tmStruckOut = */ 0,
                 /* .tm.tmPitchAndFamily = */ TMPF_TRUETYPE | TMPF_VECTOR | TMPF_FIXED_PITCH,
                 /* .tm.tmCharSet = */ ANSI_CHARSET,
+                }
             },
             {
                 /* .FontName = */ "No2Of3in1",
+                {
                 /* .tm.tmHeight = */ 12,
                 /* .tm.tmAscent = */ 12,
                 /* .tm.tmDescent = */ 0,
@@ -134,9 +141,11 @@ static fnt_test test_data[] =
                 /* .tm.tmStruckOut = */ 0,
                 /* .tm.tmPitchAndFamily = */ TMPF_TRUETYPE | TMPF_VECTOR | TMPF_FIXED_PITCH,
                 /* .tm.tmCharSet = */ ANSI_CHARSET,
+                }
             },
             {
                 /* .FontName = */ "No3Of3in1V",
+                {
                 /* .tm.tmHeight = */ 12,
                 /* .tm.tmAscent = */ 12,
                 /* .tm.tmDescent = */ 0,
@@ -157,6 +166,7 @@ static fnt_test test_data[] =
                 /* .tm.tmStruckOut = */ 0,
                 /* .tm.tmPitchAndFamily = */ FF_MODERN | TMPF_TRUETYPE | TMPF_VECTOR,
                 /* .tm.tmCharSet = */ ANSI_CHARSET,
+                }
             },
         },
     },
@@ -167,6 +177,7 @@ static fnt_test test_data[] =
         {
             {
                 /* .FontName = */ "ufaXaAlLOxCUGYJ7KN51UP2Q==",
+                {
                 /* .tm.tmHeight = */ 26,
                 /* .tm.tmAscent = */ 19,
                 /* .tm.tmDescent = */ 7,
@@ -187,6 +198,7 @@ static fnt_test test_data[] =
                 /* .tm.tmStruckOut = */ 0,
                 /* .tm.tmPitchAndFamily = */ TMPF_TRUETYPE | TMPF_VECTOR | TMPF_FIXED_PITCH,
                 /* .tm.tmCharSet = */ ANSI_CHARSET,
+                }
             },
         },
     },