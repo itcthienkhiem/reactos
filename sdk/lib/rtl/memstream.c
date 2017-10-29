itcthienkhiem/reactos

commit b3b2a23f05e5188dc1475961fcd7f036f0046d25

CID 1206831 Dereference after null check
BytesRead is an optional out parameter and must be checked before being written to.

sdk/lib/rtl/memstream.c
@@ -185,7 +185,8 @@ RtlReadMemoryStream(
 
     Stream->Current = (PUCHAR)Stream->Current + CopyLength;
 
-    *BytesRead = CopyLength;
+    if (BytesRead)
+        *BytesRead = CopyLength;
 
     return S_OK;
 }

commit 468023b01b76127828e10dd7575a204d51568f4e

[RTL] Revert b3b2a23 which introduced a regression. Spotted by Jérome

sdk/lib/rtl/memstream.c
@@ -185,8 +185,7 @@ RtlReadMemoryStream(
 
     Stream->Current = (PUCHAR)Stream->Current + CopyLength;
 
-    if (BytesRead)
-        *BytesRead = CopyLength;
+    *BytesRead = CopyLength;
 
     return S_OK;
 }