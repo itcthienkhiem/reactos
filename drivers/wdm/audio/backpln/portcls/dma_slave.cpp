itcthienkhiem/reactos

commit 99fa38809fd8ed38d4d59fa1fcf3babb3216b895

[STDUNK][PORTCLS][CMIDriver] Fix issue with operator new/delete
Fix for MSVC warning "C2323: 'operator new': non-member operator new or delete functions may not be declared static or in a namespace other than the global namespace."

See https://docs.microsoft.com/en-us/cpp/porting/visual-cpp-what-s-new-2003-through-2015?view=vs-2019 section "Overloaded operator new and operator delete"

drivers/wdm/audio/backpln/portcls/dma_slave.cpp
@@ -17,6 +17,16 @@
 class CDmaChannelInit : public IDmaChannelInit
 {
 public:
+    inline
+    PVOID
+    operator new(
+        size_t Size,
+        POOL_TYPE PoolType,
+        ULONG Tag)
+    {
+        return ExAllocatePoolWithTag(PoolType, Size, Tag);
+    }
+
     STDMETHODIMP QueryInterface( REFIID InterfaceId, PVOID* Interface);
 
     STDMETHODIMP_(ULONG) AddRef()

drivers/wdm/audio/backpln/portcls/miniport.cpp
@@ -14,6 +14,32 @@
 
 #include <debug.h>
 
+PVOID
+__cdecl
+operator new(
+    size_t Size,
+    POOL_TYPE PoolType,
+    ULONG Tag)
+{
+    return ExAllocatePoolWithTag(PoolType, Size, Tag);
+}
+
+void
+__cdecl
+operator delete(
+    PVOID ptr)
+{
+    ExFreePool(ptr);
+}
+
+void
+__cdecl
+operator delete(
+    PVOID ptr, UINT_PTR)
+{
+    ExFreePool(ptr);
+}
+
 NTSTATUS
 NTAPI
 PcNewMiniport(

drivers/wdm/audio/backpln/portcls/pin_wavecyclic.cpp
@@ -18,6 +18,16 @@ class CPortPinWaveCyclic : public IPortPinWaveCyclic,
                            public IServiceSink
 {
 public:
+    inline
+    PVOID
+    operator new(
+        size_t Size,
+        POOL_TYPE PoolType,
+        ULONG Tag)
+    {
+        return ExAllocatePoolWithTag(PoolType, Size, Tag);
+    }
+
     STDMETHODIMP QueryInterface( REFIID InterfaceId, PVOID* Interface);
 
     STDMETHODIMP_(ULONG) AddRef()

CMakeLists.txt
@@ -186,6 +186,7 @@ else()
     endif()
 
     # Other
+    add_definitions(-D_NEW_DELETE_OPERATORS_)
     if(ARCH STREQUAL "i386")
         add_definitions(-DUSE_COMPILER_EXCEPTIONS -D_USE_32BIT_TIME_T)
     elseif(ARCH STREQUAL "amd64")

drivers/wdm/audio/backpln/portcls/miniport_dmus.cpp
@@ -1708,7 +1708,7 @@ NewStream
         )
     {
         CMiniportDMusUARTStream *pStream =
-            new(PoolType) CMiniportDMusUARTStream();
+            new(PoolType, 'wNcP') CMiniportDMusUARTStream();
 
         if (pStream)
         {

drivers/wdm/audio/backpln/portcls/private.hpp
@@ -30,6 +30,13 @@
 #define PC_ASSERT_IRQL(x) PC_ASSERT(KeGetCurrentIrql() <= (x))
 #define PC_ASSERT_IRQL_EQUAL(x) PC_ASSERT(KeGetCurrentIrql()==(x))
 
+PVOID
+__cdecl
+operator new(
+    size_t Size,
+    POOL_TYPE PoolType,
+    ULONG Tag);
+
 extern
 "C"
 NTSTATUS

drivers/wdm/audio/drivers/CMIDriver/precomp.h
@@ -6,4 +6,11 @@
 
 #include "debug.hpp"
 
+PVOID
+__cdecl
+operator new(
+    size_t size,
+    POOL_TYPE pool_type,
+    ULONG tag);
+
 #endif /* _CMIDRIVER_PCH_ */

sdk/include/ddk/stdunk.h
@@ -211,7 +211,7 @@ operator delete(
     ExFreePool(ptr);
 }
 
-#endif  /* ALLOCATION_OPERATORS_DEFINED */
+#endif  /* _NEW_DELETE_OPERATORS_ */
 
 
 #else   /* Being compiled with C */

commit 1a41c4e4a98eef1179493e7fa2798369c45fb98f

[PORTCLS] Zero memory in operator new. CORE-16157
Fixes a regression from 99fa38809fd8ed38d4d59fa1fcf3babb3216b895, which
replaced the kcom.h/stdunk.h versions (which zero memory) with local
implementations (which don't).

Standard C++ does not guarantee that memory is zeroed but several classes
rely on this, in particular for their m_Ref and m_bInitialized members.

Ideally the constructors should be fixed to initialize all required members,
but that task is error-prone so for now we simply restore the previous
behavior.

drivers/wdm/audio/backpln/portcls/dma_slave.cpp
@@ -24,7 +24,10 @@ public:
         POOL_TYPE PoolType,
         ULONG Tag)
     {
-        return ExAllocatePoolWithTag(PoolType, Size, Tag);
+        PVOID P = ExAllocatePoolWithTag(PoolType, Size, Tag);
+        if (P)
+            RtlZeroMemory(P, Size);
+        return P;
     }
 
     STDMETHODIMP QueryInterface( REFIID InterfaceId, PVOID* Interface);

drivers/wdm/audio/backpln/portcls/miniport.cpp
@@ -21,7 +21,10 @@ operator new(
     POOL_TYPE PoolType,
     ULONG Tag)
 {
-    return ExAllocatePoolWithTag(PoolType, Size, Tag);
+    PVOID P = ExAllocatePoolWithTag(PoolType, Size, Tag);
+    if (P)
+        RtlZeroMemory(P, Size);
+    return P;
 }
 
 void

drivers/wdm/audio/backpln/portcls/pin_wavecyclic.cpp
@@ -25,7 +25,10 @@ public:
         POOL_TYPE PoolType,
         ULONG Tag)
     {
-        return ExAllocatePoolWithTag(PoolType, Size, Tag);
+        PVOID P = ExAllocatePoolWithTag(PoolType, Size, Tag);
+        if (P)
+            RtlZeroMemory(P, Size);
+        return P;
     }
 
     STDMETHODIMP QueryInterface( REFIID InterfaceId, PVOID* Interface);